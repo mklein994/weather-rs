835ee414bbc5ec36ca0b34f664082c9e2034630d
Matthew Klein <mklein994@gmail.com>
2018-06-11 21:11:24 -0500

    Get the weather and debug print the whole thing

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
-pub fn get_weather(api_key: String) {
-    unimplemented!();
+extern crate darksky;
+extern crate reqwest;
+
+use darksky::DarkskyReqwestRequester;
+use reqwest::Client;
+
+pub fn get_weather(
+    api_key: String,
+    lat: f64,
+    long: f64,
+) -> Result<darksky::models::Forecast, darksky::Error> {
+    let client = Client::new();
+
+    client.get_forecast(&api_key, lat, long)
+}
+
+pub fn print_weather(weather: darksky::models::Forecast) {
+    println!("{:#?}", weather);
 }
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,31 @@ extern crate dotenv;
 extern crate weather_rs;
 
 fn main() {
+    let (api_key, latitude, longitude) = get_api_params();
+
+    let weather = weather_rs::get_weather(api_key, latitude, longitude);
+
+    match weather {
+        Ok(w) => weather_rs::print_weather(w),
+        Err(err) => eprintln!("{:?}", err),
+    }
+}
+
+/// Get parameters needed to print weather from the environment.
+///
+/// Searches a file named `.env` for DARKSKY_API_KEY, DARKSKY_LATITUDE and DARKSKY_LATITUDE.
+pub fn get_api_params() -> (String, f64, f64) {
     dotenv::dotenv().ok();
 
     let api_key = dotenv::var("DARKSKY_API_KEY").expect("Missing DARKSKY_API_KEY");
 
-    weather_rs::get_weather(api_key);
+    let latitude = dotenv::var("DARKSKY_LATITUDE")
+        .map(|lat| lat.parse::<f64>().expect("Couldn't parse latitude as f64"))
+        .expect("Missing DARKSKY_API_KEY");
 
-    println!("Hello, world!");
+    let longitude = dotenv::var("DARKSKY_LONGITUDE")
+        .map(|lat| lat.parse::<f64>().expect("Couldn't parse longitude as f64"))
+        .expect("Missing DARKSKY_LONGITUDE");
+
+    (api_key, latitude, longitude)
 }
